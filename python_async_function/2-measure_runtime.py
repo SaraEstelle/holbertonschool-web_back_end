c06ce3f81e7c0cefed74a5f1206e479aaf2eac70
SaraEstelle <12152@holbertonstudents.com>
2026-04-08 11:28:10 +0200

    Changing the synthax

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,10 +1,10 @@
 #!/bin/usr/env python3
 """Module pour mesurer le temps d'exécution de wait_n."""
-
 import asyncio
 import time
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
+
 
 def measure_time(n: int, max_delay: int) -> float:
     """Mesure le temps total d'exécution de wait_n et retourne le temps moyen.
@@ -18,7 +18,9 @@ def measure_time(n: int, max_delay: int) -> float:
 
     Returns:
         float: temps moyen d'exécution par coroutine"""
-    t_satrt: float = time.perf_counter()
+    start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
-    t_end: float = time.perf_counter()
-    return (t_end - t_satrt) / n
+    end_time = time.time()
+
+    total_time = end_time - start_time
+    return total_time / n
