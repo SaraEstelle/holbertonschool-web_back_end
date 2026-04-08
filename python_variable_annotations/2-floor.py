277168476ff1d1439acba04c01bba88749091733
SaraEstelle <12152@holbertonstudents.com>
2026-04-08 09:28:43 +0200

    Ajusting the pycodestyle

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -2,6 +2,7 @@
 """Module providing a type-annotated floor function for floats."""
 import math
 
+
 def floor(n: float) -> int:
     """Return the floor of float as an integer."""
     return math.floor(n)
