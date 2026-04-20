a4818329fccdac6aa5edd0c0d550cca061cb1698
SaraEstelle <12152@holbertonstudents.com>
2026-04-20 11:09:31 +0200

    Correcting the pycodestyle

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -17,6 +17,6 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
         Returns:
             A tuple  ( startt, end) to use as slice bounds on a list.
     """
-    start =(page - 1) * page_size
+    start = (page - 1) * page_size
     end = page * page_size
-    return ( start, end)
+    return (start, end)
