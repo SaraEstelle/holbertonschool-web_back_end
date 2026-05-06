b76a1ae50a46111efeeff8e1da3131db6bcb233c
SaraEstelle <12152@holbertonstudents.com>
2026-05-06 13:38:57 +0200

    Correcting the synthax

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,11 @@
 import { uploadPhoto, createUser } from './utils.js';
-export default function handleProfileSingup() {
+export default function handleProfileSignup() {
     return Promise.all([uploadPhoto(), createUser()])
         .then((results) => {
             const [photo, user] =results;
-            console.log(`${photo.body}${user.firstName}${user.lastName}`);
+            console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
         })
         .catch(() => {
-        console.log(`Singup system offline`);
+        console.log(`Signup system offline`);
         });
 }
