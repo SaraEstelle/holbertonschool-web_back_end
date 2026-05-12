d61d8aade58f5413a48ddc9b119ecbae2ad81f80
SaraEstelle <12152@holbertonstudents.com>
2026-05-12 18:33:19 +0200

    Fix typo in Content-Type header in HTTP server response

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -34,7 +34,7 @@ function readDatabase(filePath) {
 
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-type': 'text/plain' });
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     readDatabase(databasePath)
